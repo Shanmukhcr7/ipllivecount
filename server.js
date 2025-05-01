2725cce980ab394aedfc6dd37af9e1c902634397
VEEREPALLI SHANMUKH <66630224+Shanmukhcr7@users.noreply.github.com>
2025-05-02 00:42:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
@@ -11,14 +12,22 @@ let viewers = 0;
 io.on("connection", (socket) => {
     viewers++;
     io.emit("updateViewerCount", viewers);
+    console.log(`User connected. Current viewers: ${viewers}`);
 
     socket.on("disconnect", () => {
         viewers--;
         io.emit("updateViewerCount", viewers);
+        console.log(`User disconnected. Current viewers: ${viewers}`);
     });
 });
 
-app.use(express.static("public"));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Optional: Serve the index.html on the root path
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
